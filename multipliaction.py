0edad53be64e7688269ecdbbf90f43d24abcdbad
Rabbi-Choudhury123 <120347465+Rabbi-Choudhury123@users.noreply.github.com>
2022-12-25 20:23:25 -0500

    Add files via upload

---
+++ multipliaction.py
@@ -0,0 +1,5 @@
+print("this is a program ro generate multiplication tables ")
+n = int(input("Enter the number for printing out its table: "))
+
+for intiger in range(1,13,2):
+    print(n," x ",intiger, " = ", intiger * n)
